57bf29f737c099273b047ead47489cf7a57e609a
McTrip777 <mcjakedude@gmail.com>
2019-03-21 12:24:33 -0700

    bug fixing heroku

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 
 const server = require('./server.js');
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
+const greeting = process.env.GREETING;
 
-server.listen(4000, () => {
+server.listen(port, () => {
     console.log(`Server Running on http://localhost:${port}`)
 })
